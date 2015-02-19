5314d1ed29283f5f3247e202582c5ceb83ccbda1
Vemund Santi <vemund@santi.no>
2015-02-19 22:39:45 +0100

    Sumo husker hvilken side fienden er på. Spinner ved kalibrering. Ryddet opp i koden.

--- SumoRobot/SumoRobot.ino
+++ SumoRobot/SumoRobot.ino
@@ -1,19 +1,7 @@
 #include <PLab_ZumoMotors.h>
 #include <pushbutton.h>
-#include <QTRSensors.h>
+#include <QTRSensors.h> //Needed for the sensor array
 #include <ZumoReflectanceSensorArray.h>
-
-
-// Tweak these to change speed of the Sumo
-#define REVERSE_SPEED 400
-#define TURN_SPEED 400
-#define FORWARD_SPEED 400
-#define REVERSE_DURATION 400
-#define TURN_DURATION 150
-
-
-//Activates the integrated button on the Sumo. Uses pin 12.
-Pushbutton button(ZUMO_BUTTON); 
 
 //Configures the array of IR-sensors underneath. There are 6 sensors.
 #define NUM_SENSORS 6
@@ -29,90 +17,105 @@ boolean leftState;
 boolean middleState;
 boolean turnState;
 
+
+Pushbutton button(ZUMO_BUTTON); //Activates the integrated button on the Sumo. Uses pin 12.
 PLab_ZumoMotors PLab_motors;
 ZumoReflectanceSensorArray sensors;
 
 int zumoSpeed = 150; // Speed is 150 by default. This is not very much used in the code.
 
+char lastSeenDirection = 'l';
+
 
 void setup(){
-  Serial.begin(9600);
-  pinMode(leftPin, INPUT);
-  pinMode(rightPin, INPUT);
-  pinMode(middlePin, INPUT);
-  pinMode(spinPin, INPUT);
-  
-  //Click the button once to initialize IR-sensors.
-  button.waitForButton();
-  sensors.init();
-  
-  
-  //Hvorfor er denne her?
-  delay(500);
-  
-  
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  
-  
-  //Calibrates for 10 seconds. 
-  unsigned long startTime = millis();
-  while(millis()-startTime < 10000){
-    sensors.calibrate();
-  }
-  digitalWrite(13,LOW);
-  
-  //Press the button again to start wrestling.
-  button.waitForButton();
-}
-
-
+    Serial.begin(9600);
+    pinMode(leftPin, INPUT);
+    pinMode(rightPin, INPUT);
+    pinMode(middlePin, INPUT);
+    pinMode(spinPin, INPUT);
+    
+    //Click the button once to initialize IR-sensors.
+    button.waitForButton();
+    sensors.init();
+    
+    
+    //Waits a half second before starting to calibrate the IR-sensors
+    delay(500);
+    
+    //Turns on the yellow LED to show that we are calibrating.
+    pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH);
+    
+    
+    //Calibrates for 10 seconds. 
+    unsigned long startTime = millis();
+    while(millis()-startTime < 10000){
+        PLab_motors.turnRight(200,20);
+        sensors.calibrate();
+    }
+    digitalWrite(13,LOW); //Turns off the yellow LED.
+    
+    //Press the button again to start wrestling.
+    button.waitForButton();
+}   
+    
+    
 void loop(){
-  //Checks for input from the Arduino-helper
-  rightState = digitalRead(rightPin);
-  leftState = digitalRead(leftPin);
-  middleState = digitalRead(middlePin);
-  turnState = digitalRead(spinPin);
-   
-   
-  //Checks for signals from the SumoHelper to determine what to do.
-  
-  //If the Sumohelper doesn't detect anything, start searching to the left.
-  //This should be changed to make the Sumo turn the way the other robot was last seen.
-  
-  if (turnState == HIGH){
-      PLab_motors.turnLeft(zumoSpeed,10); //Turns 10 degrees to the left.
-  }
-  
-  else if (middleState == HIGH){ //Drives forward 25 cm
-      goForward(25);
-  }
-  
-  else if (rightState == HIGH){ 
-      PLab_motors.turnRight(zumoSpeed,25); //Turn 25 degrees right, then drives forward 25 cm
-      goForward(25);
-  }
-  
-  else if (leftState == HIGH){ 
-      PLab_motors.turnLeft(zumoSpeed,25);  //Turn 25 degrees left, then drives forward 25 cm
-      goForward(25);
-  }
+    //Checks for input from the Arduino-helper
+    rightState = digitalRead(rightPin);
+    leftState = digitalRead(leftPin);
+    middleState = digitalRead(middlePin);
+    turnState = digitalRead(spinPin);
+     
+     
+    //Checks for signals from the SumoHelper to determine what to do.
+    //The sumo turns the way the enemy was last seen. Default is left.
+    if (turnState == HIGH){
+        if(lastSeenDirection == 'r'){ //If the enemy was last seen on the right, turn right.
+            PLab_motors.turnRight(zumoSpeed,10); //Turns 10 degrees to the left.
+        }
+        
+        else{ //Else, turn left.
+            PLab_motors.turnLeft(zumoSpeed,10); //Turns 10 degrees to the left.
+        }
+    }
+    
+    else if (middleState == HIGH){ //Drives forward 25 cm
+        goForward(10);
+    }
+    
+    else if (rightState == HIGH){ 
+        lastSeenDirection = 'r';
+        PLab_motors.turnRight(zumoSpeed,25); //Turn 25 degrees right, then drives forward 25 cm
+        goForward(10);
+    }
+    
+    else if (leftState == HIGH){ 
+        lastSeenDirection = 'l';
+        PLab_motors.turnLeft(zumoSpeed,25);  //Turn 25 degrees left, then drives forward 25 cm
+        goForward(10);
+    }
 }
   
 void goForward(int cm){
     //This for-loop moves 1 cm at a time, and checks the floor for black tape.
     for(int i = 0; i < cm; i++){
-        PLab_motors.forward(200,1); //Moves forward. Burde denne endres til max fart? Ja, ellers dytter den ikke med all kraft.
-        sensors.readLine(sensor_values);
+        PLab_motors.forward(200,1); //Moves forward at full speed.
+        sensors.readCalibrated(sensor_values);
         
-        if (sensor_values[0] == 1000) //Checks if there is a line on the left side.
+        if (sensor_values[0] == 1000) //Checks if there is a line on the left side. Burde verdien være 1000 her?
         {
+            lastSeenDirection = 'r';
             PLab_motors.turnRight(400,130); //Turns 130 degrees to the right with full speed.
+            goForward(10);
+            
         }
         
-        else if (sensor_values[5] == 1000) //Checks if there is a line on the right side.
+        else if (sensor_values[5] == 1000) //Checks if there is a line on the right side. Burde verdien være 1000 her?
         {
+            lastSeenDirection = 'r';
             PLab_motors.turnLeft(400,130); //Turns 130 degrees to the left with full speed.
+            goForward(10);
         }
         else if (sensor_values[1] > 900 && //Checks if the robot has driven straight onto the line.
                  sensor_values[2] > 900 && //This code will probably never be executed :(
@@ -120,6 +123,7 @@ void goForward(int cm){
                  sensor_values[4] > 900)
         {
             PLab_motors.turnLeft(400, 180); //Turns around 180 degrees with full speed.
+            goForward(10);
         }
     }
 }
